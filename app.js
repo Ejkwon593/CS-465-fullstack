1ecb636300f0f06681d5f611fc1c9e6a08858905
Eddy Kwon <Ejkwon593@users.noreply.github.com>
2025-09-11 20:51:25 -0400

    Completed Module 2 assignment

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3000;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+// Rutas
+var indexRouter = require('./app_server/routes/index');
+var travelRouter = require('./app_server/routes/traveler');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+
+// Handlebars setup
+var hbs = require('hbs');
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/health', (req, res) => res.json({ status: 'ok' }));
+// Rutas principales
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
 
-app.listen(PORT, () => {
-  console.log(`✅ Express server running at http://localhost:${PORT}`);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// ✅ Start the server
+app.listen(3000, () => {
+  console.log('Travlr app running on http://localhost:3000');
+});
+
+module.exports = app;
---
+++ app_server/controllers/traveler.js
@@ -0,0 +1,9 @@
+// traveler.js controller
+const travel = (req, res) => {
+  res.render('travel', { title: 'Travlr Getaways' });
+};
+
+module.exports = {
+  travel
+};
+
